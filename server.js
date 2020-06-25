127bf0c1896a5a2bae69e0182dadee81fa87b98f
James Sherry <james.m.sherry@googlemail.com>
2020-06-25 17:18:38 +0100

    initial files

---
+++ server.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+
+const sgMail = require("@sendgrid/mail");
+
+const { SENDGRID_API_KEY } = process.env;
+console.log("API KEY: ", SENDGRID_API_KEY);
+sgMail.setApiKey(SENDGRID_API_KEY);
+const msg = {
+  to: "james.sherry@thejump.tech",
+  from: "test@example.com",
+  subject: "Sending with Twilio SendGrid is Fun",
+  text: "and easy to do anywhere, even with Node.js",
+  html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+};
+//ES6
+// sgMail.send(msg).then(
+//   () => {},
+//   (error) => {
+//     console.error(error);
+
+//     if (error.response) {
+//       console.error(error.response.body);
+//     }
+//   }
+// );
+//ES8
+(async () => {
+  try {
+    await sgMail.send(msg);
+  } catch (error) {
+    console.error(error);
+
+    if (error.response) {
+      console.error(error.response.body);
+    }
+  }
+})();
